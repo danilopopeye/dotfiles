cd5200503e848ed28b49e1e9bf3ca61785f37c6d
Danilo Sousa <code@danilosousa.net>
2024-01-07 16:29:39 +0000

    Better `folke/trouble.nvim` config

--- neovim/lua/plugins/trouble.lua
+++ neovim/lua/plugins/trouble.lua
@@ -1,9 +1,24 @@
 return {
   "folke/trouble.nvim",
-
+  event = "VeryLazy",
   dependencies = { "nvim-tree/nvim-web-devicons" },
-  event = { "VeryLazy" },
+  keys = {
+    { "<leader>tt", "<cmd>TroubleToggle<cr>" },
+    {
+      "<leader>tw",
+      "<cmd>TroubleToggle workspace_diagnostics<cr>",
+      desc = "workspace diagnostics from the builtin LSP client"
+    },
+    {
+      "<leader>td",
+      "<cmd>TroubleToggle document_diagnostics<cr>",
+      desc = "document diagnostics from the builtin LSP client"
+    },
+  },
   opts = {
-    use_diagnostic_signs = true,
+    severity = "ERROR",          -- nil (ALL) or vim.diagnostic.severity.ERROR | WARN | INFO | HINT
+    auto_open = true,            -- automatically open the list when you have diagnostics
+    auto_close = true,           -- automatically close the list when you have no diagnostics
+    use_diagnostic_signs = true, -- enabling this will use the signs defined in your lsp client
   },
 }
