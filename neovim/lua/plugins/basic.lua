8242cc0f209624d695c004b11f8df3350a1c7b5b
Danilo Sousa <code@danilosousa.net>
2024-03-31 11:47:34 +0000

    Open alternative files for the current buffer

--- neovim/lua/plugins/basic.lua
+++ neovim/lua/plugins/basic.lua
@@ -34,4 +34,23 @@ return {
       "nvim-treesitter/nvim-treesitter",
     },
   },
+
+  -- Open alternative files for the current buffer
+  {
+    "rgroli/other.nvim",
+    event = "VeryLazy",
+    opts = {
+      mappings = {
+        "golang",
+      },
+    },
+    config = function(_, opts)
+      require("other-nvim").setup(opts)
+      vim.cmd([[
+        command! -nargs=* AT lua require('other-nvim').openTabNew(<f-args>)
+        command! -nargs=* AS lua require('other-nvim').openSplit(<f-args>)
+        command! -nargs=* AV lua require('other-nvim').openVSplit(<f-args>)
+      ]])
+    end,
+  },
 }
