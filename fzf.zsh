9e6b8cce16ace0eb09d1eeed32e130c57691eade
Danilo Sousa <code@danilosousa.net>
2024-07-13 22:45:32 -0300

    Remove old stuff

--- fzf.zsh
+++
@@ -1,8 +0,0 @@
-# Auto-completion
-# ---------------
-[[ $- == *i* ]] && source "$HOME/.zplug/repos/junegunn/fzf/shell/completion.zsh" 2> /dev/null
-
-# Key bindings
-# ------------
-source "$HOME/.zplug/repos/junegunn/fzf/shell/key-bindings.zsh"
-
